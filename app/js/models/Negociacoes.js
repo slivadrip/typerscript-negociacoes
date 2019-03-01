befc6a810114c567385601a2166931b8e778eaea
Adriano Silva <slivadrip@gmail.com>
2019-03-01 08:52:40 -0300

    Initial commit

---
+++ app/js/models/Negociacoes.js
@@ -0,0 +1,8 @@
+class Negociacoes {
+    constructor() {
+        this._negociacoes = [];
+    }
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao);
+    }
+}
---
+++ app/ts/models/Negociacoes.ts
@@ -0,0 +1,9 @@
+class Negociacoes {
+
+    private _negociacoes: Negociacao[] = [];
+
+    adiciona(negociacao: Negociacao) {
+
+        this._negociacoes.push(negociacao);
+    }
+}
