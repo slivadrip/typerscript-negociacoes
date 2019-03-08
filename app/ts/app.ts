57a9374e59b02d7ece3f0a6420f5de6e8c4177bc
Adriano Silva <slivadrip@gmail.com>
2019-03-08 10:07:05 -0300

    add jquery

--- app/ts/app.ts
+++ app/ts/app.ts
@@ -1,4 +1,2 @@
-const controller = new NegociacaoController();
-document
-    .querySelector('.form')
-    .addEventListener('submit',controller.adiciona.bind(controller));
+ const controller = new NegociacaoController();
+ $('.form').submit(controller.adiciona.bind(controller));
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,8 +1,6 @@
-declare var $: any;
-
 abstract class View<T> {
 
-    protected _elemento: any;
+    private _elemento: JQuery;
 
     constructor(seletor: string) {
 
